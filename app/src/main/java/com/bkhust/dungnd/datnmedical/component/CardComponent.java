0bc6328bcfe490508107343eb2dbbb616de2a1bd
DungND <dungnd@rikkeisoft.com>
2019-04-22 11:33:17 +0700

    init basse 10

---
+++ app/src/main/java/com/bkhust/dungnd/datnmedical/component/CardComponent.java
@@ -0,0 +1,66 @@
+package com.bkhust.dungnd.datnmedical.component;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.bkhust.dungnd.datnmedical.R;
+import com.bkhust.dungnd.datnmedical.databinding.LayoutCardBinding;
+
+/**
+ * Created by DungND on 4/22/2019.
+ */
+public class CardComponent extends CardView {
+    private Context mContext;
+    private LayoutCardBinding binding;
+
+    public CardComponent(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public CardComponent(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+        setParams(attrs);
+    }
+
+    public CardComponent(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+        setParams(attrs);
+    }
+
+    private void init(Context context) {
+        mContext = context;
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_card, this, true);
+        DataBindingUtil.bind(view);
+    }
+
+    private void setParams(AttributeSet attrs) {
+        TypedArray a = mContext.getTheme().obtainStyledAttributes(attrs, R.styleable.CardComponent, 0, 0);
+
+        String title = a.getString(R.styleable.CardComponent_cc_text);
+        setText(title);
+
+        int leftImage = a.getResourceId(R.styleable.CardComponent_cc_src_image, -1);
+        if (leftImage != -1) {
+            setImage(leftImage);
+        }
+        a.recycle();
+    }
+
+    public CardComponent setImage(int resourceId) {
+        binding.iv.setImageResource(resourceId);
+        return this;
+    }
+
+    public CardComponent setText(String text) {
+        binding.tv.setText(text);
+        return this;
+    }
+}
---
+++ app/src/main/java/com/bkhust/dungnd/datnmedical/component/ItemComponent.java
@@ -0,0 +1,70 @@
+package com.bkhust.dungnd.datnmedical.component;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.bkhust.dungnd.datnmedical.R;
+import com.bkhust.dungnd.datnmedical.databinding.LayoutItemBinding;
+
+/**
+ * Created by DungND on 4/22/2019.
+ */
+public class ItemComponent extends CardView {
+    private Context mContext;
+    private LayoutItemBinding binding;
+
+    public ItemComponent(@NonNull Context context) {
+        super(context);
+        init(context);
+    }
+
+    public ItemComponent(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+        setParams(attrs);
+    }
+
+    public ItemComponent(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+        setParams(attrs);
+    }
+
+    private void init(Context context) {
+        mContext = context;
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_item, this, true);
+        DataBindingUtil.bind(view);
+    }
+
+    private void setParams(AttributeSet attrs) {
+        TypedArray a = mContext.getTheme().obtainStyledAttributes(attrs, R.styleable.ItemComponent, 0, 0);
+
+        String title = a.getString(R.styleable.ItemComponent_ic_text);
+        setText(title);
+
+        int image = a.getResourceId(R.styleable.ItemComponent_ic_src_image, -1);
+        if (image != -1) {
+            setImage(image);
+        }
+        a.recycle();
+    }
+
+    public ItemComponent setImage(int reId) {
+        binding.ivItem.setImageResource(reId);
+        return this;
+    }
+
+    public ItemComponent setText(String title) {
+        binding.tvItem.setText(title);
+        return this;
+    }
+
+
+}
